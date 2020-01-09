9c7f6331cd6195df4b775342d8a24ecd397f02bc
IchBinHao <32353086+IchBinHao@users.noreply.github.com>
2020-01-09 10:39:54 +0800

    Update v2ray_simple.sh

--- v2ray_simple.sh
+++ v2ray_simple.sh
@@ -4,4 +4,5 @@ wget https://raw.githubusercontent.com/flyzy2005/ss-fly/master/v2ray.sh && chmod
 #rm /etc/v2ray/config.json
 mv v2ray_script/config.json /etc/v2ray/config.json
 service v2ray restart
+bash v2ray_script/bbr.sh
 
