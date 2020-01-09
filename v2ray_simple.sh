ec6f1a09d4b54523708e7c3009f54d6f509efcbe
IchBinHao <32353086+IchBinHao@users.noreply.github.com>
2020-01-09 10:31:59 +0800

    Update v2ray_simple.sh

--- v2ray_simple.sh
+++ v2ray_simple.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/flyzy2005/ss-fly/master/v2ray.sh && chmod +x v2ray.sh
-git clone https://github.com/IchBinHao/v2ray_script.git
 ./v2ray.sh
 rm /etc/v2ray/config.json
 mv v2ray_script/config.json /etc/v2ray/config.json
